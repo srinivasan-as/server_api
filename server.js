f598d066d8dec7e3bc23cb753b10e64bc1bb5cee
srinivasan-as <sriinivasan.a@gmail.com>
2023-02-28 21:03:50 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// routes
+const todo = require("./routes/todo");
+
+// connect database
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// initialize middleware
+app.use(express.json({ extended: false }));
+app.get("/", (req, res) => res.send("Server up and running"));
+
+// use routes
+app.use("/api/todo", todo);
+
+// setting up port
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
